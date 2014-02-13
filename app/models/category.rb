6ec0dc3a194412f55e543bdc4716099ea5043c3b
kristysnet <kristy@kristys.net>
2014-02-12 21:48:25 -0800

    Added category scope to only return top level

--- app/models/category.rb
+++ app/models/category.rb
@@ -4,4 +4,6 @@ class Category < ActiveRecord::Base
   belongs_to :parent,   class_name: '::Category'
   has_many   :children, class_name: '::Category', foreign_key: 'parent_id', primary_key: 'category_id'
 
+  scope :top, -> { where(parent_id: nil) }
+
 end
--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -6,4 +6,16 @@ describe Category do
   it { should belong_to(:parent) }
   it { should have_many(:children) }
 
+  describe '#top' do
+    let(:categories) { (1..5).map{ |i| FactoryGirl.create(:category) } }
+    let(:sub_categories) { (1..5).map{ |i| FactoryGirl.create(:sub_category) } }
+    it 'displays only top level categories' do
+      categories.each do |category|
+        expect(Category.top).to include(category)
+      end
+      sub_categories.each do |subcategory|
+        expect(Category.top).to_not include(subcategory)
+      end
+    end
+  end
 end
