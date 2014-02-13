0d8004eec717f7098f89c082fa727208709166bc
kristysnet <kristy@kristys.net>
2014-02-12 21:01:28 -0800

    Associate accounts with transactions

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,5 +1,6 @@
 class Account < ActiveRecord::Base
 
   validates :name, presence: true, uniqueness: true
+  has_many :transactions, dependent: :destroy
 
 end
--- spec/models/account_spec.rb
+++ spec/models/account_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 
 describe Account do
 
-  it{should validate_presence_of(:name)}
+  it { should validate_presence_of(:name) }
+  it { should have_many(:transactions) }
 
 end
