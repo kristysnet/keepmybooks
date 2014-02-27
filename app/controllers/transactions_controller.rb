54108ab3b5d47056d1ce8482299058ec54252a3d
kristysnet <kristy@kristys.net>
2014-02-26 21:29:28 -0800

    Transaction create method (via json)

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,8 +1,19 @@
 class TransactionsController < ApplicationController
   before_filter :load_account
+  respond_to :html, :json
 
   def index
     @transactions = Transaction.where(account_id: @account.id).order('transaction_date desc')
+  end
+
+  def create
+    @transaction = Transaction.new(transaction_params)
+    @transaction.account_id = @account.id
+    if @transaction.save
+      respond_with @transaction, status: :created, location: nil
+    else
+      respond_with({errors: @transaction.errors}, status: :unprocessable_entity, location: nil)
+    end
   end
 
   private
@@ -10,5 +21,10 @@ class TransactionsController < ApplicationController
   def load_account
     @account ||= Account.find(params[:account_id])
   end
+
+  def transaction_params
+    params.require(:transaction).permit(:transaction_date, :category_id, :amount, :receiver, :note)
+  end
+
 
 end
--- spec/controllers/transactions_controller_spec.rb
+++ spec/controllers/transactions_controller_spec.rb
@@ -14,4 +14,34 @@ describe TransactionsController do
     end
   end
 
+  describe '#create' do
+    let(:json) { JSON.parse(response.body) }
+    context 'successfully creates a transaction' do
+      let(:transaction) { build(:transaction) }
+      it 'responds with a new transaction' do
+        post :create, account_id: transaction.account_id, transaction: transaction.attributes.symbolize_keys, format: :json
+        expect(json['account_id']).to eq(transaction.account_id)
+        expect(json['receiver']).to eq(transaction.receiver)
+        expect(json['amount']).to eq(transaction.amount)
+      end
+      it 'creates a new account' do
+        expect {
+          post :create, account_id: transaction.account_id, transaction: transaction.attributes.symbolize_keys, format: :json
+        }.to change(Transaction, :count).by(1)
+      end
+    end
+    context 'cannot create account' do
+      let(:account) { create(:account) }
+      it 'does not save the new account' do
+        expect {
+          post :create, account_id: account.id, transaction: attributes_for(:transaction), format: :json
+        }.to_not change(Transaction,:count)
+      end
+      it 'responds with an error message' do
+        post :create, account_id: account.id, transaction: attributes_for(:invalid_transaction), format: :json
+        expect(json['errors']).to include({ 'receiver' => ['can\'t be blank'] })
+      end
+    end
+  end
+
 end
